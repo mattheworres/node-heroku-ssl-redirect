a4b43e7d084fd67d41fb5175dae597b00c46397c
Matthew Orres <matthew.orres@gmail.com>
2017-10-16 10:00:46 -0400

    Use host in place of hostname for port purposes when necessary.

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ module.exports = function(environments, status) {
   status = status || 302;
   return function(req, res, next) {
     if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+      //Hostname does not include port, host does.
+      var hostnameValue = req.hostname === req.host ? req.hostname : req.host;
+
       if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect(status, 'https://' + req.hostname + req.originalUrl);
+        res.redirect(status, 'https://' + hostnameValue + req.originalUrl);
       }
       else {
         next();
